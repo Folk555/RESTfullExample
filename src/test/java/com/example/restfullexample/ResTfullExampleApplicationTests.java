0e74748f998e51d3bbe313baddfdf8822961c8ea
Folk555 <sanya.turulin.98@list.ru>
2022-06-02 14:44:20 +0300

    RESTfull example

---
+++ src/test/java/com/example/restfullexample/ResTfullExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfullexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfullExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
